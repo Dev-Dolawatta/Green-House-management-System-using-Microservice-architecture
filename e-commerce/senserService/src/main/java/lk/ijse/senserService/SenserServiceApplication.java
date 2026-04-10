37b040c268a78598d7778f150a85ab666bc37b39
Dev-Dolawatta <.dtdolawatta@gmail.com>
2026-04-10 17:39:24 +0530

    initialize scheduler in senser service

---
+++ e-commerce/senserService/src/main/java/lk/ijse/senserService/SenserServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.senserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenserServiceApplication.class, args);
+	}
+
+}
---
+++ e-commerce/senserService/src/test/java/lk/ijse/senserService/SenserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.senserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
