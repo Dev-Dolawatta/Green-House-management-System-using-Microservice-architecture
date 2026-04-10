69f3022285d8d6b4eb0436786a9f45a91b73709b
Dev-Dolawatta <.dtdolawatta@gmail.com>
2026-04-10 18:03:28 +0530

    declare senser service entity

---
+++ e-commerce/automationService/src/main/java/lk/ijse/automationService/entity/AutomationLog.java
@@ -0,0 +1,18 @@
+package lk.ijse.automationService.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class AutomationLog {
+    @Id
+    private String id;
+
+    private String zoneId;
+
+    private double temperature;
+
+    private String action;
+}
